9ec118d8e6a37f5a515f97a2426ea365bac11881
Krushna Shinde <krushnashinde534@gmail.com>
2026-09-22 06:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (83.22%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[v] = i
+        return []
+        
